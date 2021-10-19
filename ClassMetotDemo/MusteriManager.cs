bd4db5a75c39ccd4f2fb4a7ac560868066218ef3
NAZIF BALCI <nzf_blc5757@hotmail.com>
2021-10-19 13:00:06 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+     class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine( "Müsteri başarıyla eklenmiştir"+ "--" + musteri.Ad + " " +musteri.Soyad  );
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Silindi"+ "--" + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Güncellendi" + "--" + musteri.Ad + " " + musteri.Soyad );
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Nazif";
+            musteri1.Soyad = "Balcı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Betül";
+            musteri2.Soyad = "Güleryüz";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Güncelle(musteri2);
+            
+        }
+    }
+}
